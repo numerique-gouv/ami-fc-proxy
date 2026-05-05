ed40e5eb155ee31390b2679d4c30f980d1de82ef
Lauréline Guérin <zebuline@entrouvert.com>
2026-05-05 16:17:20 +0200

    fix scalingo deployments (remove heroku uv buildpack)

--- bin/start.sh
+++ bin/start.sh
@@ -2,10 +2,13 @@
 
 PORT="${PORT:-8000}"
 HOSTNAME="${HOSTNAME:-127.0.0.1}"
+RUN="uv run"
 
 if [ -z "$CONTAINER" ]
 then
   SSL="${SSL:---ssl-keyfile=ssl-key.pem --ssl-certfile=ssl-cert.pem}"
+  # Don't use uv
+  RUN=""
 fi
 
 uv run litestar run -p ${PORT} -H ${HOSTNAME} ${RELOAD} ${DEBUG} ${SSL}
