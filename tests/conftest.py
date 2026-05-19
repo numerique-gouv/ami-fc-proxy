8463db0f44827f4bcd12aa21c79ffabaa7a3e3ba
Lauréline Guérin <zebuline@entrouvert.com>
2026-05-19 13:16:33 +0200

    ami-fi: use FileStore (#708)

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,22 @@
+from pathlib import Path
+
 import pytest
 from litestar import Litestar
+from litestar.stores.file import FileStore
 from litestar.testing import TestClient
 
 from app import create_app
 
 
+@pytest.fixture()
+def oidc_session_store(tmp_path: Path) -> FileStore:
+    return FileStore(path=tmp_path / "oidc_sessions")
+
+
 @pytest.fixture
-def app() -> Litestar:
+def app(oidc_session_store: FileStore) -> Litestar:
     """Create app with test database connection."""
-    app_ = create_app()
+    app_ = create_app(stores={"oidc_sessions": oidc_session_store})
     app_.debug = True
     return app_
 
